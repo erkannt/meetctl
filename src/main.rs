5d8a5b7d345aa87352caf890a4a58c2458579d22
Daniel Haarhoff <daniel@rknt.de>
2023-05-05 08:47:55 +0100

    Spec out desired behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+
+
+#[derive(Parser)]
+#[command(arg_required_else_help = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Starts Meet in a Chrom[e|ium] instance with debug access
+    Launch{},
+    /// Takes a room name, alias or url
+    Join{room: String},
+    /// Starts sharing your screen
+    Share{},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Launch { }) => {
+            println!("Not implemented")
+        }
+        Some(Commands::Join { room }) => {
+            println!("Not implemented")
+        }
+        Some(Commands::Share { }) => {
+            println!("Not implemented")
+        }
+        None => {}
+    }
+
 }
