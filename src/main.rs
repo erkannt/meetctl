d950af84852194ad0c04af2c3f15b01120b9c50b
Daniel Haarhoff <daniel@rknt.de>
2023-05-05 17:32:03 +0100

    Launch browser and access it via debug port

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,19 @@ fn main() {
 
     match &cli.command {
         Some(Commands::Launch {}) => {
-            println!("Not implemented")
+            std::process::Command::new("chromium")
+                .args([
+                    "--remote-debugging-port=9222",
+                    "--no-first-run",
+                    "--no-default-browser-check",
+                ])
+                .spawn()
+                .expect("Failed to launch");
+
+            let browser_info =
+                backoff::retry(backoff::ExponentialBackoff::default(), get_browser_info)
+                    .expect("Failed to get browser info");
+            println!("{}", browser_info)
         }
         Some(Commands::Join { room }) => {
             println!("Not implemented ({})", room)
@@ -32,4 +44,9 @@ fn main() {
         }
         None => {}
     }
+}
+
+fn get_browser_info() -> Result<String, backoff::Error<reqwest::Error>> {
+    let text = reqwest::blocking::get("http://127.0.0.1:9222/json/version")?.text()?;
+    return Ok(text);
 }
